9983895c3e823d4efbc8431eb53f8a72bde792e8
wolf76 <iamnarey@gmail.com>
2023-01-09 16:58:50 +0530

    new: custom accordion and dropdown-icon components

---
+++ components/commons/CustomAccordion.vue
@@ -0,0 +1,260 @@
+<template>
+  <div class="custom-accordion">
+    <div class="custom-accordion__container">
+      <div
+        class="custom-accordion__item"
+        v-for="(item, index) in customItems"
+        :key="item.header"
+      >
+        <div
+          class="custom-accordion__item__header flex justify-between"
+          :class="{ active: item.active }"
+          @click="activateBody(index)"
+        >
+          <div
+            v-if="itemHeights[index]"
+            class="custom-accordion__item__header__left flex items-center"
+          >
+            {{ item.header }}
+          </div>
+          <div class="custom-accordion__item__header__right flex items-center">
+            <div class="mr-2 desktop">
+              {{ item.period }}
+            </div>
+
+            <DropdownIcon :openState="item.active" />
+          </div>
+        </div>
+        <div
+          class="custom-accordion__item__body"
+          :class="{ visible: item.active }"
+          :style="[item.active ? { height: itemHeights[index] + 'px' } : '']"
+        >
+          <div class="custom-accordion__item__body__wrapper">
+            <div
+              class="custom-accordion__item__body__content md:flex justify-between md:flex-row-reverse"
+            >
+              <div
+                class="custom-accordion__item__body__content__img pb-6 md:pb-0"
+              >
+                <img :src="getImage(item.body.image)" :alt="item.name" />
+              </div>
+              <div class="custom-accordion__item__body__content__data pr-4">
+                <div
+                  class="custom-accordion__item__body__content__data__company md:flex"
+                >
+                  <div
+                    class="custom-accordion__item__body__content__data__company__location flex items-center mt-1 md:mt-0"
+                  >
+                    <img
+                      src="~/assets/images/map-pin-icon-dark.png"
+                      alt="map-pin"
+                    />
+                    <div class="ml-1">{{ item.body.location }}</div>
+                  </div>
+                  <div
+                    class="custom-accordion__item__body__content__data__company__site mt-1 md:mt-0 md:ml-4"
+                  >
+                    <a
+                      :href="item.body.website"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      class="flex items-center"
+                    >
+                      <img src="~/assets/images/url-icon-dark.png" alt="url" />
+                      <div class="ml-1">{{ item.body.website }}</div>
+                    </a>
+                  </div>
+                </div>
+                <div
+                  class="custom-accordion__item__body__content__data__desc pt-4 md:pt-6 pb-6 md:pb-8 pl-1"
+                >
+                  {{ item.body.description }}
+                </div>
+                <div
+                  class="custom-accordion__item__body__content__data__tech flex items-center flex-wrap"
+                >
+                  <div
+                    v-for="tag in item.body.tags"
+                    :key="tag"
+                    class="tag-bubble"
+                  >
+                    {{ tag }}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import DropdownIcon from './DropdownIcon.vue'
+
+export default {
+  components: {
+    DropdownIcon,
+  },
+
+  props: {
+    items: Array,
+  },
+
+  data() {
+    return {
+      accordionItems: Array,
+      itemHeights: [],
+    }
+  },
+
+  computed: {
+    customItems() {
+      this.accordionItems = [...this.items]
+      return this.accordionItems
+    },
+  },
+
+  mounted() {
+    setTimeout(() => {
+      let divElement = document.querySelectorAll(
+        '.custom-accordion__item__body__wrapper'
+      )
+
+      divElement.forEach((element) => {
+        this.itemHeights.push(element.clientHeight)
+      })
+    }, 300)
+  },
+
+  methods: {
+    activateBody(index) {
+      this.accordionItems[index].active = !this.accordionItems[index].active
+      this.accordionItems.forEach((element, ind) => {
+        if (ind !== index) {
+          element.active = false
+        }
+      })
+    },
+
+    getImage(imgName) {
+      return require(`~/assets/images/${imgName}`)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.custom-accordion {
+  max-width: 100%;
+  display: flex;
+  margin-right: auto;
+  margin-left: auto;
+  position: relative;
+
+  @media (min-width: 768px) {
+    max-width: 80%;
+  }
+
+  &__container {
+    width: 100%;
+  }
+
+  &__item {
+    &__header {
+      background-color: #f9a826;
+      cursor: pointer;
+      position: relative;
+      padding: 15px 20px;
+      border-radius: 5px;
+      color: #fff;
+      font-weight: 700;
+
+      &.active {
+        background-color: #f9a826;
+      }
+    }
+
+    &__body {
+      height: 10px;
+      overflow: hidden;
+      color: #fff;
+
+      transition: all cubic-bezier(0.4, 0, 0.2, 1) 0.4s;
+
+      &__wrapper {
+        padding: 20px 0px;
+      }
+
+      &__content {
+        background-color: #e6e6e6;
+        color: #505051;
+        border-radius: 5px;
+        padding: 15px 20px;
+        font-size: 14px;
+        line-height: 18px;
+
+        &__data {
+          width: 100%;
+
+          @media (min-width: 768px) {
+            width: 75%;
+          }
+
+          &__company {
+            font-weight: 700;
+
+            &__location {
+              img {
+                width: 20px;
+                height: 20px;
+              }
+            }
+
+            &__site {
+              img {
+                width: 20px;
+                height: 20px;
+              }
+            }
+          }
+
+          &__tech {
+            .tag-bubble {
+              padding: 5px 15px;
+              border-radius: 20px;
+              background-color: #8a8a8a;
+              margin: 0 2px 2px;
+              color: #fff;
+              font-size: 12px;
+            }
+          }
+        }
+
+        &__img {
+          @media (min-width: 768px) {
+            width: 25%;
+          }
+
+          img {
+            max-width: 200px;
+
+            @media (min-width: 768px) {
+              width: 100%;
+              height: 100%;
+              object-fit: contain;
+            }
+          }
+        }
+      }
+
+      &.visible {
+        visibility: visible !important;
+        opacity: 1 !important;
+      }
+    }
+  }
+}
+</style>
---
+++ components/commons/DropdownIcon.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="dropdown-icon" :class="{ open: openState }">
+    <span></span>
+    <span></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    openState: Boolean,
+    default: false,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.dropdown-icon {
+  position: relative;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  span {
+    display: inline-block;
+    position: absolute;
+    width: 10px;
+    height: 2px;
+    background: #fff;
+    border-radius: 40px;
+    transition: 0.5s;
+
+    &:nth-child(1) {
+      transform: rotate(45deg);
+      left: 7px;
+    }
+
+    &:nth-child(2) {
+      transform: rotate(-45deg);
+      right: 7px;
+    }
+  }
+
+  &.open {
+    span {
+      &:nth-child(1) {
+        transform: rotate(135deg);
+      }
+
+      &:nth-child(2) {
+        transform: rotate(-135deg);
+      }
+    }
+  }
+}
+</style>
