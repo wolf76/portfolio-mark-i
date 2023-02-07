a0ac97914a8a1bd79c805ce74f50ce833cd91574
wolf76 <iamnarey@gmail.com>
2023-02-07 16:41:03 +0530

    fix: add tooltip for skills icons

--- components/Skills.vue
+++ components/Skills.vue
@@ -4,10 +4,11 @@
     <CustomBorder />
     <div class="naresh-portfolio-skills__content mt-10">
       <div
-        class="naresh-portfolio-skills__content__skill"
+        class="naresh-portfolio-skills__content__skill tooltip"
         v-for="skill in skills"
         :key="skill"
       >
+        <span class="tooltiptext capitalize"> {{ skill }} </span>
         <img :src="getImage(skill)" alt="" />
       </div>
     </div>
@@ -76,5 +77,44 @@ export default {
       }
     }
   }
+}
+
+.tooltip {
+  position: relative;
+  display: inline-block;
+  border-bottom: 1px dotted var(--secondary-color);
+  transition: 1s ease;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  background-color: var(--secondary-color);
+  color: var(--secondary-color-light-two);
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+  bottom: 110%;
+  left: 50%;
+  margin-left: -60px;
+
+  &::after {
+    content: ' ';
+    position: absolute;
+    top: 100%; /* At the bottom of the tooltip */
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: var(--secondary-color) transparent transparent transparent;
+  }
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
 }
 </style>
